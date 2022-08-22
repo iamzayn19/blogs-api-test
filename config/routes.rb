1db35788c1ca6bb22e27e2f48215323a97e7f9f7
zain <iamzayn19@gmail.com>
2022-08-22 14:34:37 +0500

    Add sessions controller and paths from cblogs-api

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  resources :home
-  resources :blogs
-  resources :comments
-  root 'home#index'
-  get '/search/blog', to: 'blogs#search_blog'
+  # resources :home
+  # resources :blogs
+  # resources :comments
+  # root 'home#index'
+  # get '/search/blog', to: 'blogs#search_blog'
+  resources :blogs do
+    resources :comments
+    resources :blog_likes
+  end 
+  post '/session/new', to: 'sessions#create'
+  post '/registration/new', to: 'registrations#create'
+  get '/confirmation/new', to: 'confirmations#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
