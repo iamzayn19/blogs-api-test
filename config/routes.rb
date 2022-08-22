981f77814a1181001055adc1b438bf8d9ff8da7d
zain <iamzayn19@gmail.com>
2022-08-22 16:39:40 +0500

    Add login button on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   resources :registrations
   resources :confirmations
   resources :home
-  root 'sessions#new'
+  root 'blogs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
