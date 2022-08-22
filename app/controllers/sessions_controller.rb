80185ea349fa5f317cc0a8da02b0e74b3cfe06fb
zain <iamzayn19@gmail.com>
2022-08-22 15:16:24 +0500

    Rescue 401 on failed authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,29 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
+     @body = RestClient.post "https://blogs-rest-api.herokuapp.com/api/v1/auth/login?email=#{params[:email]}&password=#{params[:password]}",
+     @headers = {accept: :json,  'x-api-key' => ENV["SYGIC_API_KEY"]}
+     
+     @body = JSON.parse(@body)
+     
+     if @body.empty? == false
+      render json: @body
+     end
+
+     
+     
+     rescue RestClient::Unauthorized => exception
+      render json: "Invalid email/password"
+      return
+      
+
+     
     
   end 
+
+  def destroy
+  end 
+  
 end
